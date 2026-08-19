76657c7b0351d384b5e8364828140d6931cd40ac
pxoqxo <pxoqxo@atomicmail.io>
2026-08-19 22:40:07 +0530

    PresetPaths added.

---
+++ 2faConsole/PresetPaths.cs
@@ -0,0 +1,8 @@
+﻿namespace _2faConsole
+{
+    internal static class PresetPaths
+    {
+        internal const string Data = "data";
+        internal static readonly string Profiles = Path.Combine(Data, "profiles.json");
+    }
+}
