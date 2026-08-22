653ac3bab655ac01582492009b9f8f48b4a1ee0e
pxoqxo <pxoqxo@atomicmail.io>
2026-08-22 11:51:17 +0530

    Models added.

---
+++ 2faConsole/Models.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _2faConsole
+{
+    public sealed class OtpFileDm
+    {
+        [JsonPropertyName("Otps")]
+        public List<OtpDm> Otps { set; get; } = new List<OtpDm>();
+    }
+    public sealed class OtpDm
+    {
+        [JsonPropertyName("Id")]
+        public string Id { set; get; } = string.Empty;
+
+        [JsonPropertyName("Name")]
+        public string Name { set; get; } = string.Empty;
+
+        [JsonPropertyName("Secret")]
+        public string Secret { set; get; } = string.Empty;
+
+        [JsonPropertyName("Type")]
+        public OtpType Type { set; get; } = OtpType.Unknown;
+
+        [JsonPropertyName("Digits")]
+        public int Digits { set; get; } = 0;
+
+        [JsonPropertyName("Counter")]
+        public long Counter { set; get; } = 0;
+    }
+    public enum OtpType
+    {
+        Unknown = 0,
+        Totp = 1,
+        Hotp = 2
+    }
+}
