301a3916c7bc5d4527a2182369e71a1c02d1bbab
pxoqxo <pxoqxo@atomicmail.io>
2026-08-22 11:58:44 +0530

    Program class updated with main loop.

--- 2faConsole/Program.cs
+++ 2faConsole/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2faConsole;
+
+while (true)
+{
+    Console.Clear();
+    byte? option = OtpManager.SelectOption();
+
+    if (option == 0)
+    {
+        Environment.Exit(0);
+    }
+    else if (option == 1)
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("No option is avaibale!");
+        Console.WriteLine("Please retry...");
+        Thread.Sleep(500);
+    }
+}
