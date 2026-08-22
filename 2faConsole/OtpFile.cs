b75fd5234e9a5c61c379386beb3f52b2684c802f
pxoqxo <pxoqxo@atomicmail.io>
2026-08-22 12:21:19 +0530

    OtpFile added.

---
+++ 2faConsole/OtpFile.cs
@@ -0,0 +1,28 @@
+﻿using Pxoqxo.Quick;
+
+namespace _2faConsole
+{
+    public static class OtpFile
+    {
+        public static OtpFileDm? Read()
+        {
+            string? json = QuickFile.Read(PresetPaths.Otps);
+            if (json == null)
+            {
+                return null;
+            }
+
+            return QuickJson.FromJson<OtpFileDm>(json);
+        }
+        public static bool Write(OtpFileDm otpFileDm)
+        {
+            string? json = QuickJson.ToJson(otpFileDm);
+            if (json == null)
+            {
+                return false;
+            }
+
+            return QuickFile.Write(PresetPaths.Otps, json);
+        }
+    }
+}
