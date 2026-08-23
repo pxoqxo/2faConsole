0b1e49ba536ca5623114d2bacf3079a9b6349fa7
pxoqxo <pxoqxo@atomicmail.io>
2026-08-23 20:58:45 +0530

    New options added in Program.cs.

--- 2faConsole/Program.cs
+++ 2faConsole/Program.cs
@@ -2,21 +2,39 @@
 
 while (true)
 {
-    Console.Clear();
     byte? option = OtpManager.SelectOption();
 
     if (option == 0)
     {
+        Console.Clear();
+        Console.WriteLine("Thank you for using 2FA Console. Goodbye!");
         Environment.Exit(0);
     }
     else if (option == 1)
     {
-
+        OtpManager.AddOtp();
+    }
+    else if (option == 2)
+    {
+        OtpManager.EditOtp();
+    }
+    else if (option == 3)
+    {
+        OtpManager.RemoveOtp();
+    }
+    else if (option == 4)
+    {
+        OtpManager.ViewOtp();
+    }
+    else if (option == 5)
+    {
+        OtpManager.ListOtp();
     }
     else
     {
-        Console.WriteLine("No option is avaibale!");
-        Console.WriteLine("Please retry...");
-        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine("That option is not available.");
+        Console.WriteLine("Please press any key to try again.");
+        Console.ReadKey();
     }
 }
